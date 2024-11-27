5d9d5c42738c644fa9bc0029f42f958089d6f13a
Jordan DeDea <jordandedea@gmail.com>
2024-11-27 12:53:54 -0600

    Add scroll offset and other options to vim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,20 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+local opt = vim.opt
+
+-- Prevent line wrapping 
+opt.wrap = false
+
+-- Enable relative line numbers
+opt.relativenumber = true
+
+-- complete the longest common match, and allow tabbing the results to fully complete them
+opt.wildmode = "longest:full,full"
+
+-- Scroll offset (how many lines / characters of padding basically when scrolling)
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+-- Ask for confirmation instead of erroring when quitting without saving
+-- opt.confirm = true
