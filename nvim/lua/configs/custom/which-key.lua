01563341fde1a2fb4bab6f29e2e3b335c1c5a7e6
Jordan DeDea <jordandedea@gmail.com>
2024-11-29 19:23:47 -0600

    Add more plugins to nvim

---
+++ nvim/lua/configs/custom/which-key.lua
@@ -0,0 +1,6 @@
+local wk = require("which-key")
+
+wk.add({
+  { "gS", mode = "n", desc = "Split onto multiple lines", icon = "≡" },
+  { "gJ", mode = "n", desc = "Join multiple lines into one line", icon = "⊕" },
+})
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,36 +1,84 @@
 return {
   {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("configs.custom.which-key")
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
       "EmranMR/tree-sitter-blade"
     },
     opts = {
-  		ensure_installed = {
-  		 "vim", "lua", "vimdoc", "blade",
-       "html", "css", "php", "rust",
+      ensure_installed = {
+        "vim", "lua", "vimdoc", "blade",
+        "html", "css", "php", "rust",
         "javascript", "typescript"
-  		},
+      },
       highlight = {
         enable = true,
       },
-  	},
+      autotag = {
+        enable = true,
+      },
+    },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
       require("configs.custom.treesitter")
     end,
   },
+  -- Add more languages
+  { "sheerun/vim-polyglot" },
+
+  -- Enable * searching with visually selected text
+  { "nelstrom/vim-visual-star-search" },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+        per_filetype = {}  -- leaving empty since we want default behavior
+      })
+    end,
+  },
+
+  -- Allows for gS to split or gJ to join lines
+  {
+    "AndrewRadev/splitjoin.vim",
+    event = "VeryLazy",
+  },
+
+  -- Fix indentation when pasting
+  {
+    "ku1ik/vim-pasta",
+    event = "VeryLazy",
+  }
 }
+
