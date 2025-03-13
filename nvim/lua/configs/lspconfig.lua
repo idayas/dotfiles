9a5a1fe8c8764d86892e952519d1bb26cf13bac8
Jordan DeDea <jordandedea@gmail.com>
2025-03-13 10:06:07 -0500

    Fix phpactor warning about missing return types

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -15,24 +15,18 @@ local servers = {
 }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- Set up intelephense separately with priority
 lspconfig.phpactor.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
   priority = 100, -- High priority for PHP
   init_options = {
+    ["language_server.diagnostics_on_update"] = false,
+    ["language_server.diagnostics_on_open"] = false,
+    ["language_server.diagnostics_on_save"] = false,
     ["language_server_phpstan.enabled"] = false,
     ["language_server_psalm.enabled"] = false,
-    ["language_server.diagnostics.enabled"] = true,
-    ["language_server.diagnostics.php.enabled"] = true,
-    -- Hide most warnings regarding missing type declarations
-    ["language_server.diagnostics.php.phpDocMissingReturn"] = false,
-    ["language_server.diagnostics.php.missingReturnType"] = false,
-    ["language_server.diagnostics.php.missingType"] = false,
-    ["language_server.diagnostics.php.missingVisibility"] = false,
-    ["language_server.diagnostics.php.isMixed"] = false,
-    ["language_server.diagnostics.php.deprecatedType"] = false,
+    ["language_server.diagnostic_ignore_codes"] = {"missing_return_type"}
   },
 }
 
