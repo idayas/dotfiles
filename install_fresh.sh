e5edb1c50fdcbaafcb37a3de846ca143215c8c41
Jordan DeDea <jordandedea@gmail.com>
2025-09-03 08:18:08 -0500

    Re-order fresh install and add bun

--- install_fresh.sh
+++ install_fresh.sh
@@ -2,15 +2,20 @@
 
 DOTFILES=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
+# Developer Tools
+
+#  ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update && brew upgrade
 
-# Install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
+# Fonts
+brew install --cask font-fantasque-sans-mono-nerd-font
 
-# Neovim
+# Neovim Nightly
 brew install neovim --HEAD
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
@@ -26,14 +31,16 @@ ln -sf $DOTFILES/git/gitconfig $HOME/.gitconfig
 # Git Ignore
 ln -sf $DOTFILES/git/gitignore_global $HOME/.gitignore_global
 
-# fonts
-brew install --cask font-fantasque-sans-mono-nerd-font
-
 # kitty
 brew install --cask kitty
 rm -rf $HOME/.config/kitty
 ln -s $DOTFILES/kitty $HOME/.config/kitty
 
+# bun
+brew tap oven-sh/bun
+brew install bun
+
+# Quality of Life
 # enable ctrl + cmd to drag
 defaults write -g NSWindowShouldDragOnGesture -bool true
 
