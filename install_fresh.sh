438e7eacf5ef74031fcae8af45a7e0ad8368b7fb
Jordan DeDea <jordandedea@gmail.com>
2025-09-03 09:08:12 -0500

    General maintenance and cleanup of installation files and zsh. Add composer

--- install_fresh.sh
+++ install_fresh.sh
@@ -3,44 +3,46 @@
 DOTFILES=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 # Developer Tools
-
-#  ohmyzsh
+## ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
 
-# Install Homebrew
+## Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update && brew upgrade
 
-# Fonts
+## Fonts
 brew install --cask font-fantasque-sans-mono-nerd-font
 
-# Neovim Nightly
+## Neovim Nightly
 brew install neovim --HEAD
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
 
-# Ohmyposh
+## Ohmyposh
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 rm -rf $HOME/.config/ohmyposh
 ln -s $DOTFILES/ohmyposh $HOME/.config/ohmyposh
 
-# Git
+## Git
 ln -sf $DOTFILES/git/gitconfig $HOME/.gitconfig
 
-# Git Ignore
+## Git Ignore
 ln -sf $DOTFILES/git/gitignore_global $HOME/.gitignore_global
 
-# kitty
+## kitty
 brew install --cask kitty
 rm -rf $HOME/.config/kitty
 ln -s $DOTFILES/kitty $HOME/.config/kitty
 
-# bun
+## bun
 brew tap oven-sh/bun
 brew install bun
 
+## Composer (php)
+brew install composer
+
 # Quality of Life
-# enable ctrl + cmd to drag
+## enable ctrl + cmd to drag
 defaults write -g NSWindowShouldDragOnGesture -bool true
 
